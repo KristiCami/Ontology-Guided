f0855f082b2f1156212ada6e9e8a5a417f7dce40
KristiCami <cami.kristi@icloud.com>
2025-08-01 09:43:02 +0300

    ..

--- tests/test_data_loader.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from data_loader import DataLoader
-
-def test_preprocess_text():
-    loader = DataLoader()
-    text = "The ATM logs transactions. Users withdraw cash."
-    sentences = loader.preprocess_text(text)
-    assert sentences == ["The ATM logs transactions.", "Users withdraw cash."]
