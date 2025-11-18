4373e805266a6b40164de1a55abed5facd7e18a3
KristiCami <cami.kristi@icloud.com>
2025-11-18 15:27:23 +0200

    .

--- ontology_pipeline/namespaces.py
+++
@@ -1,8 +0,0 @@
-"""Namespace helpers used across the pipeline."""
-from rdflib import Namespace
-
-BASE = Namespace("http://example.org/atm#")
-RBO = Namespace("http://example.org/rbo#")
-LO = Namespace("http://example.org/lo#")
-
-__all__ = ["BASE", "RBO", "LO"]
